46585f0b6c75f3c7ad767cdc44eea8372c3a7932
Isaac Rogers <therealplato@gmail.com>
2016-08-28 09:57:34 -0400

    start profiling

--- cmd/muid.go
+++ cmd/muid.go
@@ -3,21 +3,20 @@ package main
 import (
 	"encoding/hex"
 	"flag"
-	"fmt"
-	"log"
-	"net/http"
 	_ "net/http/pprof"
+	"os"
+	"runtime/pprof"
 
 	"github.com/therealplato/muid"
 )
 
 func main() {
-	go func() {
-		log.Println(http.ListenAndServe("localhost:6060", nil))
-	}()
-
+	f, _ := os.Create("cpuprofile.out")
+	g, _ := os.Create("memprofile.out")
+	pprof.StartCPUProfile(f)
+	defer pprof.StopCPUProfile()
 	var count = flag.Int("n", 1000, "generate this many ID's")
-	var quiet = flag.Bool("q", false, "mute stdout")
+	// var quiet = flag.Bool("q", false, "mute stdout")
 	var bytesT = flag.Int("sizets", 8, "use this many bytes for LSBs of nanosecond timestamp")
 	var bytesMID = flag.Int("sizeid", 8, "use this many bytes for machine ID")
 	var midhex = flag.String("machineid", "1234567890abcdef", "hexadecimal machine id")
@@ -37,8 +36,10 @@ func main() {
 			panic(err)
 		}
 		out[i] = tmp
-		if !*quiet {
-			fmt.Printf("%x\n", tmp)
-		}
+		// if !*quiet {
+		// 	fmt.Printf("%x\n", tmp)
+		// }
 	}
+	pprof.WriteHeapProfile(g)
+	g.Close()
 }
