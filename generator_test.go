9d5ce825b8713b1a4b7ddf4703b75557cf253da0
Isaac Rogers <therealplato@gmail.com>
2016-08-28 18:51:23 -0400

    test repeated runs are different

--- generator_test.go
+++ generator_test.go
@@ -19,8 +19,13 @@ func TestGenerator(t *testing.T) {
 		assert.Equal(t, 3, g.SizeTS)
 		assert.Equal(t, 3, g.SizeMID)
 		assert.Equal(t, machineid, g.MachineID)
-		t.Run("when machineid is blank", func(t *testing.T) {
+		t.Run("errors when machineid is blank", func(t *testing.T) {
 			g, err = NewGenerator(3, 0, []byte{})
+			assert.NotNil(t, err)
+			assert.Nil(t, g)
+		})
+		t.Run("errors when machineid is wrong length", func(t *testing.T) {
+			g, err = NewGenerator(3, 2, []byte{1})
 			assert.NotNil(t, err)
 			assert.Nil(t, g)
 		})
@@ -45,61 +50,13 @@ func TestGenerator(t *testing.T) {
 		assert.WithinDuration(t, time.Now(), ts, time.Millisecond)
 	})
 
-	// 		t.Run("machineid portion of id", func(t *testing.T) {
-	// 			t.Run("is left zero padded when short", func(t *testing.T) {
-	// 				id, err := generate(16, 8, machineid)
-	// 				assert.Nil(t, err)
-	// 				expected := MUID{0, 0, 0, 0, 0, 0x66, 0x6f, 0x6f}
-	// 				assert.Equal(t, expected, id[8:])
-	// 			})
-	// 			t.Run("is truncated, keeping right bytes, when long", func(t *testing.T) {
-	// 				machineid := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	// 				id, err := generate(16, 8, machineid)
-	// 				assert.Nil(t, err)
-	// 				expected := MUID([]byte{
-	// 					2, 3, 4, 5, 6, 7, 8, 9,
-	// 				})
-	// 				assert.Equal(t, expected, id[8:])
-	// 			})
-	// 		})
-	// 		t.Run("repeated runs yield unique ids", func(t *testing.T) {
-	// 			id1, _ := generate(16, 8, machineid)
-	// 			id2, _ := generate(16, 8, machineid)
-	// 			id3, _ := generate(16, 8, machineid)
-	// 			assert.NotEqual(t, id1, id2)
-	// 			assert.NotEqual(t, id2, id3)
-	// 			assert.NotEqual(t, id3, id1)
-	// 		})
-	// 	})
-	// 	t.Run("it returns an error when called with a blank machine id", func(t *testing.T) {
-	// 		id, err := generate(16, 8, []byte{})
-	// 		assert.Nil(t, id)
-	// 		assert.NotNil(t, err)
-	// 	})
-	// }
-
-	// func TestPadOrTrim(t *testing.T) {
-	// 	in := []byte{1, 2, 3, 4, 5}
-
-	// 	t.Run("given correct size input, returns same input", func(t *testing.T) {
-	// 		out := padOrTrim(in, 5)
-	// 		assert.Equal(t, []byte{1, 2, 3, 4, 5}, out)
-	// 	})
-
-	// 	t.Run("given long input, returns size bytes from the right", func(t *testing.T) {
-	// 		out := padOrTrim(in, 3)
-	// 		assert.Equal(t, []byte{3, 4, 5}, out)
-	// 	})
-
-	// 	t.Run("given short input, returns size bytes, zero prefixed", func(t *testing.T) {
-	// 		in := []byte{1, 2, 3, 4, 5}
-	// 		out := padOrTrim(in, 8)
-	// 		assert.Equal(t, []byte{0, 0, 0, 1, 2, 3, 4, 5}, out)
-	// 	})
-
-	// 	t.Run("given nil input, returns size bytes, zero prefixed", func(t *testing.T) {
-	// 		in := []byte{}
-	// 		out := padOrTrim(in, 8)
-	// 		assert.Equal(t, []byte{0, 0, 0, 0, 0, 0, 0, 0}, out)
-	// 	})
+	t.Run("repeated runs yield unique ids", func(t *testing.T) {
+		g, err = NewGenerator(3, 3, machineid)
+		id1 := g.Generate()
+		id2 := g.Generate()
+		id3 := g.Generate()
+		assert.NotEqual(t, id1, id2)
+		assert.NotEqual(t, id2, id3)
+		assert.NotEqual(t, id3, id1)
+	})
 }
