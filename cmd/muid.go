c54d16d7a2b36c3df15452207d374c0fb34135fb
Isaac Rogers <therealplato@gmail.com>
2016-08-28 17:33:24 -0400

    add quiet and profile flags

--- cmd/muid.go
+++ cmd/muid.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/hex"
 	"flag"
+	"fmt"
 	_ "net/http/pprof"
 	"os"
 	"runtime/pprof"
@@ -11,16 +12,22 @@ import (
 )
 
 func main() {
-	f, _ := os.Create("cpuprofile.out")
-	g, _ := os.Create("memprofile.out")
-	pprof.StartCPUProfile(f)
-	defer pprof.StopCPUProfile()
 	var count = flag.Int("n", 1000, "generate this many ID's")
-	// var quiet = flag.Bool("q", false, "mute stdout")
+	var quiet = flag.Bool("q", false, "mute stdout")
 	var bytesT = flag.Int("sizets", 8, "use this many bytes for LSBs of nanosecond timestamp")
 	var bytesMID = flag.Int("sizemid", 8, "use this many bytes for machine ID")
 	var midhex = flag.String("mid", "1234567890abcdef", "hexadecimal machine id")
+	var profile = flag.Bool("profile", false, "enable cpu and memory profiling")
 	flag.Parse()
+
+	var f, g *os.File
+	if *profile {
+		f, _ = os.Create("cpuprofile.out")
+		g, _ = os.Create("memprofile.out")
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
 	machineID, err := hex.DecodeString(*midhex)
 	if err != nil {
 		panic("machineID is invalid bytes: " + err.Error())
@@ -28,19 +35,20 @@ func main() {
 	if len(machineID) != *bytesMID {
 		panic("machineID is wrong number of hex bytes")
 	}
+
 	m, err := muid.NewGenerator(*bytesT, *bytesMID, machineID)
 	if err != nil {
 		panic(err)
 	}
-	// for i := 0; i < *count; i++ {
-	// 	tmp := m.Generate()
-	// 	out[i] = tmp
-	// 	// if !*quiet {
-	// 	// 	fmt.Printf("%x\n", tmp)
-	// 	// }
-	// }
+
 	out := m.Bulk(*count)
-	pprof.WriteHeapProfile(g)
-	g.Close()
-	_ = out
+	if !*quiet {
+		for _, muid := range out {
+			fmt.Printf("%x\n", muid)
+		}
+	}
+	if *profile {
+		pprof.WriteHeapProfile(g)
+		g.Close()
+	}
 }
